6227892346b25433b6902f42549eca008a8c0cf8
ziauddinziya07 <ziauddinziya07@gmail.com>
2023-06-30 12:01:10 +0530

    first commit

---
+++ src/components/Compliments.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Compliments.css";
+
+function Compliments() {
+    return (
+        <section id="compliments">
+            <h1>With Best Compliments From:</h1>
+            <h5>Al-Haaj Abdul Mateen,</h5>
+            <h5>K. Muhammad Zubair and Brothers,</h5>
+            <h5>Ghulshan -e- Khanbu,</h5>
+            <h5>Near & Dear - Relatives & Friends.</h5>
+        </section>
+    );
+}
+
+export default Compliments;
---
+++ src/components/InviteNote.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/InviteNote.css";
+
+function InviteNote() {
+    return (
+        <section id="invite-note">
+            <div className="from">
+                <h5>An Invitation from:</h5>
+                <h2>Mrs. Al-Haaj Khanbu Abdul Aleem Sahab(Late)</h2>
+
+                <h5 className="address">Retd. Teacher and Pesh Imam</h5>
+                <h5 className="address mb-3">
+                    H.No.: 1/564, Bharpet,
+                    <br /> Khooni Mohallah, Adoni,
+                    <br /> Kurnool Dist., AP - 518 301.
+                </h5>
+                <p className="address">
+                    Contact: <br />
+                    +91 94415 80407, <br />
+                    +91 98856 34576.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default InviteNote;
